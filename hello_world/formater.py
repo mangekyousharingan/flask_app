b3cb64ebf5f48cd101a6199a7c0ca07a2850f323
mangekyousharingan <tester@tester-m>
2019-06-08 09:09:18 +0000

    solve error

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -21,7 +21,7 @@ def get_formatted(msg, imie, format):
 
 
 def format_to_json(msg, imie):
-    return str({"imie":imie, "ms":msg})
+    return str({"imie":imie, "mgs":msg})
 
 
 def plain_text(msg, imie):
